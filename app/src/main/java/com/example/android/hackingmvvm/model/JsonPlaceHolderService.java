ef0bc7be894a80f4f0441fa6b521bfee2ff00ed4
pranit <pranitsawant01@gmail.com>
2016-09-12 22:38:55 +0530

    adding retrofit service class

---
+++ app/src/main/java/com/example/android/hackingmvvm/model/JsonPlaceHolderService.java
@@ -0,0 +1,12 @@
+package com.example.android.hackingmvvm.model;
+
+/**
+ * Description Please
+ *
+ * @author pranit
+ * @version 1.0
+ * @since 11/9/16
+ */
+
+public interface JsonPlaceHolderService {
+}
